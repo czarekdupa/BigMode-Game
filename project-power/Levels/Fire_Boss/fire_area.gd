26578894869a53fec493a0f6b18669788459396c
czarekdupa <czarekbudzinski@yahoo.com>
2025-01-31 22:42:38 -0600

    added particles to fire area

--- project-power/Levels/Fire_Boss/fire_area.gd
+++ project-power/Levels/Fire_Boss/fire_area.gd
@@ -4,46 +4,50 @@ extends Area2D
 var deal_damage = false
 var current_player : CharacterBody2D
 var default_particles : Dictionary = {}
+var boss = CharacterBody2D
+var has_boss = false
 
 func _ready() -> void:
 	damage_per_second = damage_per_second * $Timer .wait_time
 	#sets default_particles
-	default_particles.direction = $fire_particles.direction
 	default_particles.spread = $fire_particles.spread
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	default_particles.speed_scale = $fire_particles.speed_scale
+	default_particles.life_time = $fire_particles.lifetime
+	default_particles.amount = $fire_particles.amount
 func _process(delta: float) -> void:
-	pass
-
+	if has_boss:
+		$fire_particles.direction = (boss.global_position - global_position).normalized()
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.owner.is_in_group("player"):
 		current_player = area.owner
 		area.owner.take_damage(damage_per_second)
-		print("took " + str(damage_per_second) + " damage from" + $".".name)
 		$Timer.start()
 
 func _on_area_exited(area: Area2D) -> void:
 	if area.owner.is_in_group("player"):
 		current_player = null
 		$Timer.stop()
-	
 
 func _on_timer_timeout() -> void:
 	current_player.take_damage(damage_per_second)
 	print("took " + str(damage_per_second) + " damage from" + $".".name)
 
-
-
+#boss detection
 func _on_boss_detector_body_entered(body: Node2D) -> void:
 	if body.is_in_group("boss"):
-		$fire_particles.direction = body.global_position
 		$fire_particles.spread = 10
-		
-#cmon
+		$fire_particles.speed_scale = 7
+		$fire_particles.lifetime = 12
+		$fire_particles.amount = 200
+		has_boss = true
+		boss = body
+
 func _on_boss_detector_body_exited(body: Node2D) -> void:
 	if body.is_in_group("boss"):
-		for key in default_particles.keys():
-			#var i = $fire_particles.get(key)
-			#i = default_particles[key]
-			pass
+		has_boss = false
+		$fire_particles.spread = default_particles.spread
+		$fire_particles.speed_scale = default_particles.speed_scale
+		$fire_particles.lifetime = default_particles.life_time
+		$fire_particles.amount = default_particles.amount
+	pass
