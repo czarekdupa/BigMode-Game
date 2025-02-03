91d8b5131c851c3b386ec110d5bc52b8caae5f22
czarekdupa <czarekbudzinski@yahoo.com>
2025-02-02 18:02:06 -0600

    moai boss almost done

--- project-power/Levels/Moai_Boss/moai_boss.gd
+++ project-power/Levels/Moai_Boss/moai_boss.gd
@@ -1,14 +1,15 @@
-extends CharacterBody2D
+extends AnimatableBody2D
 var hp = 100
+@export var move_to = Marker2D
 
 func _ready():
 	$CanvasLayer/Health_Bar.value = hp
 	$CanvasLayer/Health_Bar.max_value = hp
 
 
-func _physics_process(delta: float) -> void:
-
-	move_and_slide()
+func final_move():
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "global_position", move_to.global_position, 2)
 
 
 func take_damage(damage):
